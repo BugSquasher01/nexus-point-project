054ebc2c86b1d87367527c316f5ffc2944f76981
unknown <wowfie123@gmail.com>
2021-03-07 23:02:16 +0000

    Edit dummy data to  display correct car info on listings grid

--- store/index.js
+++ store/index.js
@@ -1,38 +1,97 @@
 export const state = () => ({
-    fooddata: [{
-        name: "Taco bout Eating Tacos",
-        deliveryTime: 25,
-        meal: ["lunch", "dinner"],
-        rating: 4.7,
-        freeDelivery: true,
+    cardata: [{
         id: "f71400ed-26b1-43d0-940c-7cabd3546dc6",
-        menu: [{
-            item: "Taco",
-            img: "taco-taco.jpg",
-            price: 3.45,
+        listings: [{
+            item: "2014 (64) Mercedes-Benz",
+            shortDescription: "CLA 250e Coupe Shooting Break",
+            img: "row1_image1.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23300,
             id: "e85dbacc-f721-44e6-af8c-eda2835e2e50",
-            options: ["Al Pastor", "Carnitas", "Lengua", "Carne Asada", "Pollo"],
-            addOns: ["none", "sour cream", "avocado", "pico de gallo", "queso"],
-            description: "Soft tortilla, your choice of meat, onions, cilantro & salsa"
+            options: ["DAB Digital radio", "USB port", "Harman/Kardon logic 7 surround sound system + 12 speakers"],
+            addOns: ["Carbon effect trim", "Black air vent surrounds with red insert"],
+            description: ""
           },
           {
-            item: "Quesadilla",
-            img: "taco-quesadilla.png",
-            price: 7.0,
+            item: "2020 (20) Peugeot 208",
+            shortDescription: "1.2 PureTech 100 GT-Line",
+            img: "row1_image2.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
             id: "e956a779-941f-41ef-874e-d5f6a6c6e0cb",
-            options: ["Plain", "Carne Asada", "Pollo"],
-            addOns: ["none", "sour cream", "guacamole", "pico de gallo"],
-            description: "Flour tortilla, melted cheese & optional meat choice"
+            options: ["DAB Digital radio", "USB port", "Harman/Kardon logic 7 surround sound system + 12 speakers"],
+            addOns: ["Carbon effect trim", "Black air vent surrounds with red insert"],
+            description: ""
           },
           {
-            item: "Burrito",
-            img: "taco-burrito.jpg",
-            price: 6.59,
+            item: "2019 (19) Renault Zoe",
+            shortDescription: "i R135 Rapid Charge ZE50",
+            img: "row1_image3.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
             id: "ecd7c207-acd7-4b58-8685-894ba622949e",
-            options: ["Al Pastor", "Carnitas", "Lengua", "Carne Asada", "Pollo"],
-            addOns: ["none", "sour cream", "avocado", "pico de gallo", "queso"],
-            description: "Any meat, rice, beans, onions, cilantro & salsa"
+            options: ["DAB Digital radio", "USB port", "Harman/Kardon logic 7 surround sound system + 12 speakers"],
+            addOns: ["Carbon effect trim", "Black air vent surrounds with red insert"],
+            description: ""
           }
+        ]
+      },
+      {
+        id: "e1e569e5-7b75-42cf-8fc3-7638d6c940f8",
+        listings: [{
+            item: "2020 (20) Peugeot 208",
+            shortDescription: "1.2 PureTech 100 GT-Line",
+            img: "row2_image1.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
+            id: "7644eba9-d6a8-466f-bd49-781bc005cfe5",
+            description: ""
+          },
+          {
+            item: "Blank for now",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            id: "70ceec44-3ccb-4f92-b149-0ebe2c10704b",
+            description: ""
+          },
+          {
+            item: "2014 (64) Mercedes-Benz",
+            shortDescription: "CLA 250e Coupe Shooting Break",
+            img: "row2_image2.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
+            id: "b27f4d8e-7235-42f9-b0a7-98e76a1afaf0",
+            description: ""
+          }
+        ]
+      },
+      {
+        id: "1f9500d2-fd27-422e-acc6-48198d0de5c2",
+        listings: [{
+            item: "2019 (19) Renault Zoe",
+            shortDescription: "i R135 Rapid Charge ZE50",
+            img: "row3_image1.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
+            id: "22672b93-2c65-4fd9-b151-683f7eb7df4a",
+            description: ""
+          },
+          {
+            item: "2014 (64) Mercedes-Benz",
+            shortDescription: "CLA 250e Coupe Shooting Break",
+            img: "row3_image2.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
+            id: "3a6da02c-2354-4eb8-af07-f9f2d26fa7c2",
+            description: ""
+          },
+          {
+            item: "2020 (20) Peugeot 208",
+            shortDescription: "1.2 PureTech 100 GT-Line",
+            img: "row3_image3.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
+            description: ""
+          },
         ]
       },
       {
@@ -42,126 +101,33 @@ export const state = () => ({
         rating: 4.5,
         freeDelivery: false,
         id: "e1e569e5-7b75-42cf-8fc3-7638d6c940f8",
-        menu: [{
-            item: "Build Your Own",
-            img: "pizza-plain.jpg",
-            addOns: [
-              "None",
-              "Sausage",
-              "Pepperoni",
-              "Tomatoes",
-              "Mushrooms",
-              "Onion",
-              "Green Pepper",
-              "Extra Cheese"
-            ],
-            price: 17.99,
+        listings: [{
+            item: "2014 (64) Mercedes-Benz",
+            shortDescription: "CLA 250e Coupe Shooting Break",
+            img: "row3_image2.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
             id: "7644eba9-d6a8-466f-bd49-781bc005cfe5",
-            description: "Design your own pizza"
+            description: ""
           },
           {
-            item: "MeatLovers",
-            img: "pizza-meat.jpg",
-            price: 17.99,
-            addOns: [
-              "None",
-              "Sausage",
-              "Pepperoni",
-              "Tomatoes",
-              "Mushrooms",
-              "Onion",
-              "Green Pepper",
-              "Extra Cheese"
-            ],
+            item: "2020 (20) Peugeot 208",
+            shortDescription: "1.2 PureTech 100 GT-Line",
+            img: "row3_image3.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
             id: "70ceec44-3ccb-4f92-b149-0ebe2c10704b",
-            description: "A Carnivore’s delight. Pepperoni, Sausage, Salami & Canadian Bacon."
+            description: ""
           },
           {
-            item: "Veggie",
-            img: "pizza-veggie.jpg",
-            price: 17.99,
-            addOns: [
-              "None",
-              "Sausage",
-              "Pepperoni",
-              "Tomatoes",
-              "Mushrooms",
-              "Onion",
-              "Green Pepper",
-              "Extra Cheese"
-            ],
+            item: "2019 (19) Renault Zoe",
+            shortDescription: "i R135 Rapid Charge ZE50",
+            img: "row3_image1.jpg",
+            monthlyPrice: 550.90 + " /mo (PCP)",
+            fullPrice: 23.300,
             id: "b27f4d8e-7235-42f9-b0a7-98e76a1afaf0",
-            description: "Tomatoes, Olives, Garlic, Onions, Mushrooms & Bells."
-          }
-        ]
-      },
-      {
-        name: "All that and Dim Sum",
-        deliveryTime: 35,
-        meal: ["breakfast", "lunch", "dinner"],
-        rating: 4.9,
-        freeDelivery: true,
-        id: "1f9500d2-fd27-422e-acc6-48198d0de5c2",
-        menu: [{
-            item: "Shrimp Dumplings",
-            img: "dimsum-shrimp.jpg",
-            price: 6.49,
-            addOns: [
-              "None",
-              "Steamed Sesame Ball",
-              "Mango Pudding",
-              "Sweet Tofu"
-            ],
-            id: "22672b93-2c65-4fd9-b151-683f7eb7df4a",
-            description: "Whole shrimp in a translucent wrapper."
-          },
-          {
-            item: "Soup Dumplings",
-            img: "dimsum-soup.jpg",
-            price: 9.99,
-            addOns: [
-              "None",
-              "Steamed Sesame Ball",
-              "Mango Pudding",
-              "Sweet Tofu"
-            ],
-            id: "3a6da02c-2354-4eb8-af07-f9f2d26fa7c2",
-            description: "Dumplings filled with pork, crab meat and broth."
-          },
-          {
-            item: "Stuffed Eggplant",
-            img: "dimsum-eggplant.jpg",
-            price: 5.59,
-            addOns: [
-              "None",
-              "Steamed Sesame Ball",
-              "Mango Pudding",
-              "Sweet Tofu"
-            ],
-            id: "cadd2903-b9cb-4053-9720-c096cc95e8a5",
-            description: "Chinese eggplant filled with ground shrimp in sauce"
+            description: ""
           }
         ]
       }
     ]
 })
-
-// export const getters = {
-//     getterValue: state => {
-//         return state.value
-//     }
-// }
-
-
-// export const mutations = {
-//     updateValue: (state, payload) => {
-//         state.value = payload
-//     }
-// }
-
-
-// export const actions = {
-//     updateActionValue({ commit }) {
-//         commit('updateValue', payload)
-//     }
-// }
