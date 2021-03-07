7418976fd9c3b1be0cd708909eb8372d7d3e68a3
unknown <wowfie123@gmail.com>
2021-03-07 16:51:22 +0000

    Create AppMenu.vue component to house navigation menu

---
+++ components/AppMenu.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav>
+        <ul>
+            <li><nuxt-link exact to="/"> Home </nuxt-link></li>
+            <li><nuxt-link exact to="/about"> About </nuxt-link></li>
+        </ul>
+    </nav>
+</template>
