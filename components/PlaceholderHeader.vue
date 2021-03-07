d943cbb3c6844b1d94ce0f9428fba2da921a5e3b
unknown <wowfie123@gmail.com>
2021-03-07 23:09:44 +0000

    Tidy up placeholder header code structure

--- components/PlaceholderHeader.vue
+++ components/PlaceholderHeader.vue
@@ -1,9 +1,7 @@
 <template>
     <nav>
         <ul>
-            <li><nuxt-link exact to="/"> Home </nuxt-link></li>
-            <li><nuxt-link exact to="/about"> About </nuxt-link></li>
             <p>Template Header</p>
         </ul>
     </nav>
-</template>
+</template> 
