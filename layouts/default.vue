4cbe8a56f6922a3f1c5320dfaeb6b2a00014e569
unknown <wowfie123@gmail.com>
2021-03-07 16:53:12 +0000

    Import AppMenu.vue for use & add navigation bar to default page layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
+<div>
+  <div class='content'>
+    <AppMenu />
     <Nuxt />
   </div>
+</div>
 </template>
+
+<script>
+import AppMenu from '@/components/AppMenu.vue'
+
+export default {
+  components: {
+    AppMenu
+  },
+}
+</script>
+
 
 <style>
 html {
