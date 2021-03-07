a902d9e8d7fc1fadf17c893489e59e5fb7658ad5
unknown <wowfie123@gmail.com>
2021-03-07 17:22:40 +0000

    Updated AppMenu.vue to display Template Header text

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -3,6 +3,7 @@
         <ul>
             <li><nuxt-link exact to="/"> Home </nuxt-link></li>
             <li><nuxt-link exact to="/about"> About </nuxt-link></li>
+            <p>Template Header</p>
         </ul>
     </nav>
 </template>
