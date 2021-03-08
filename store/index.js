8bc6bfe6a53c9be3291f8e1065341be790f11f79
unknown <wowfie123@gmail.com>
2021-03-08 01:33:31 +0000

    Remove unused dummy data & add new boolean values

--- store/index.js
+++ store/index.js
@@ -3,35 +3,36 @@ export const state = () => ({
         id: "f71400ed-26b1-43d0-940c-7cabd3546dc6",
         listings: [{
             item: "2014 (64) Mercedes-Benz",
+            valueYourCar: true,
+            calculateFinance: false,
             shortDescription: "CLA 250e Coupe Shooting Break",
             img: "row1_image1.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
             fullPrice: 23300,
             id: "e85dbacc-f721-44e6-af8c-eda2835e2e50",
-            options: ["DAB Digital radio", "USB port", "Harman/Kardon logic 7 surround sound system + 12 speakers"],
-            addOns: ["Carbon effect trim", "Black air vent surrounds with red insert"],
             description: ""
           },
           {
             item: "2020 (20) Peugeot 208",
+            valueYourCar: true,
+            calculateFinance: true,
             shortDescription: "1.2 PureTech 100 GT-Line",
             img: "row1_image2.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "e956a779-941f-41ef-874e-d5f6a6c6e0cb",
-            options: ["DAB Digital radio", "USB port", "Harman/Kardon logic 7 surround sound system + 12 speakers"],
-            addOns: ["Carbon effect trim", "Black air vent surrounds with red insert"],
             description: ""
           },
           {
             item: "2019 (19) Renault Zoe",
+            valueYourCar: true,
+            viewButton: true,
+            calculateFinance: true,
             shortDescription: "i R135 Rapid Charge ZE50",
             img: "row1_image3.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "ecd7c207-acd7-4b58-8685-894ba622949e",
-            options: ["DAB Digital radio", "USB port", "Harman/Kardon logic 7 surround sound system + 12 speakers"],
-            addOns: ["Carbon effect trim", "Black air vent surrounds with red insert"],
             description: ""
           }
         ]
@@ -40,25 +41,32 @@ export const state = () => ({
         id: "e1e569e5-7b75-42cf-8fc3-7638d6c940f8",
         listings: [{
             item: "2020 (20) Peugeot 208",
+            valueYourCar: true,
+            calculateFinance: true,
             shortDescription: "1.2 PureTech 100 GT-Line",
             img: "row2_image1.jpg",
+            valueYourCar: true,
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "7644eba9-d6a8-466f-bd49-781bc005cfe5",
             description: ""
           },
           {
-            item: "Blank for now",
-            monthlyPrice: 550.90 + " /mo (PCP)",
+            // item: "Blank for now",
+            img: "Valuation.jpg",
+            valueYourCar: false,
+            // monthlyPrice: 550.90 + " /mo (PCP)",
             id: "70ceec44-3ccb-4f92-b149-0ebe2c10704b",
             description: ""
           },
           {
             item: "2014 (64) Mercedes-Benz",
+            valueYourCar: true,
+            calculateFinance: false,
             shortDescription: "CLA 250e Coupe Shooting Break",
             img: "row2_image2.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "b27f4d8e-7235-42f9-b0a7-98e76a1afaf0",
             description: ""
           }
@@ -68,50 +76,55 @@ export const state = () => ({
         id: "1f9500d2-fd27-422e-acc6-48198d0de5c2",
         listings: [{
             item: "2019 (19) Renault Zoe",
+            valueYourCar: true,
+            calculateFinance: true,
             shortDescription: "i R135 Rapid Charge ZE50",
             img: "row3_image1.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "22672b93-2c65-4fd9-b151-683f7eb7df4a",
             description: ""
           },
           {
             item: "2014 (64) Mercedes-Benz",
+            valueYourCar: true,
+            calculateFinance: false,
             shortDescription: "CLA 250e Coupe Shooting Break",
             img: "row3_image2.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "3a6da02c-2354-4eb8-af07-f9f2d26fa7c2",
             description: ""
           },
           {
             item: "2020 (20) Peugeot 208",
+            valueYourCar: true,
+            calculateFinance: true,
             shortDescription: "1.2 PureTech 100 GT-Line",
             img: "row3_image3.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             description: ""
           },
         ]
       },
       {
-        name: "A Real Pizza Work",
-        deliveryTime: 40,
-        meal: ["lunch", "dinner"],
-        rating: 4.5,
-        freeDelivery: false,
         id: "e1e569e5-7b75-42cf-8fc3-7638d6c940f8",
         listings: [{
             item: "2014 (64) Mercedes-Benz",
+            valueYourCar: true,
+            calculateFinance: false,
             shortDescription: "CLA 250e Coupe Shooting Break",
             img: "row3_image2.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "7644eba9-d6a8-466f-bd49-781bc005cfe5",
             description: ""
           },
           {
             item: "2020 (20) Peugeot 208",
+            valueYourCar: true,
+            calculateFinance: true,
             shortDescription: "1.2 PureTech 100 GT-Line",
             img: "row3_image3.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
@@ -120,10 +133,12 @@ export const state = () => ({
           },
           {
             item: "2019 (19) Renault Zoe",
+            valueYourCar: true,
+            calculateFinance: true,
             shortDescription: "i R135 Rapid Charge ZE50",
             img: "row3_image1.jpg",
             monthlyPrice: 550.90 + " /mo (PCP)",
-            fullPrice: 23.300,
+            fullPrice: 23300,
             id: "b27f4d8e-7235-42f9-b0a7-98e76a1afaf0",
             description: ""
           }
