23a79c259fdd956e0db195ac197c1428c9f22b0a
unknown <wowfie123@gmail.com>
2021-03-08 03:23:40 +0000

    Add conditional rendering of page elements & updated code comments

--- components/CarListingsGrid.vue
+++ components/CarListingsGrid.vue
@@ -1,25 +1,34 @@
 <template>
     <section class='carListingsMainGrid'>
         <div v-for='data in cardata' :key='data.id'>
+
+            <!--Main listings feed grid view -->
             <div class='row'>
                 <div 
                     v-for='carlisting in data.listings'
                     :key='carlisting.id'
                     class='carImage'
-                    :style='`background: url(${carlisting.img}) no-repeat; width: 100%; height: auto;`'>
+                    :style='`background: url(${carlisting.img}) no-repeat; max-width: 100%; height: auto;`'>
 
+                    <!--Extra car information at bottom of each card -->
                     <div v-if='carlisting.valueYourCar' class='iteminfoContainer' >
                         <div class='itemInfo'>
                             <h3 class='carListingTitle'>{{ carlisting.item }}</h3>
                             <p class='carListingDescription'>{{ carlisting.shortDescription }}</p>
                             <p class='carListingMonthlyPrice'>£{{ carlisting.monthlyPrice }}</p>
                             <p class='carListingFullPrice'>£{{ carlisting.fullPrice }}
-                            <a v-if='calculateFinance' class='calculateFinanceLink' href='#'>Calculate Finance</a></p>
+                            <a v-if='carlisting.calculateFinance' class='calculateFinanceLink' href='#'>Calculate Finance</a>
+                            <a v-else class='calculateFinanceLink' href='#'>Calculate</a>
+                            </p>
                         </div>
+                    <!--End of extra car information at bottom of each card -->
+
+                        <!-- View car button (currently not in use) -->
                         <button v-if='carlisting.viewButton' class='viewCarButton'>View</button>
                     </div>
                 </div>
             </div>
+            <!--End of main listings feed grid view -->
         </div>
     </section>
 </template>
@@ -36,5 +45,4 @@ export default {
 </script>
 
 <style>
-
 </style>
